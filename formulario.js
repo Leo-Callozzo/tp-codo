1dfbc7fdddfa27c13ac5f92bf7b201aa7cede7b2
Leonardo Callozzo <callozzoleonardo@gmail.com>
2024-05-26 20:16:31 -0300

    modificaciones y mejoras en la validacion del form

--- formulario.js
+++ formulario.js
@@ -6,9 +6,11 @@ enviar.addEventListener("click", function(){
     let mail = document.getElementById("email").value;
     let mensaje = document.getElementById("mensajeEnviado");
     let msjError = document.getElementById("mensajeError");
+    
 
-         if (mail.indexOf('@')>=(0)){
-            //console.log("ok");
+         
+        if(validateEmail(mail))
+        {    
             formulario.reset();
             mensaje.classList.add("mostrar");
             msjError.classList.remove("mostrar");
@@ -21,3 +23,19 @@ enviar.addEventListener("click", function(){
          }
     
 }) 
+
+
+
+function validateEmail(email){
+                
+	
+	var validEmail =  /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+	
+	if( validEmail.test(email) ){
+		
+		return true;
+	}else{
+		
+		return false;
+	}
+} 
